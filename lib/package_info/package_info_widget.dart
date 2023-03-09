97164037ce6f0780ffbece32b3203491575f88ce
Mwangi Micha <michameiu@gmail.com>
2023-03-09 20:50:23 +0300

    Implement widget builder for package info

--- lib/package_info/package_info_widget.dart
+++ lib/package_info/package_info_widget.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_utils/package_info/package_info_controller.dart';
 import 'package:get/get.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 class PackageInfoWidget extends StatelessWidget {
   TextStyle? style;
 
+  Widget Function(BuildContext context, PackageInfo? packageInfo)?
+      widgetBuilder;
+
   PackageInfoWidget({
     super.key,
     this.style,
+    this.widgetBuilder,
   });
   PackageInfoController packageInfoController =
       Get.put(PackageInfoController());
@@ -15,6 +20,9 @@ class PackageInfoWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Obx(() {
+      if (widgetBuilder != null) {
+        return widgetBuilder!(context, packageInfoController.packageInfo.value);
+      }
       return Text(
         packageInfoController.packageInfo.value?.version ?? "Loading...",
         style: style,
