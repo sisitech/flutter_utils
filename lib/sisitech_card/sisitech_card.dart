94a37531be0023d1290c200c1c71ed132b17c3d3
Ali Dennis <chivatsi@sisitech.com>
2024-02-20 20:12:03 +0300

    hot fix for sisitech cards

--- lib/sisitech_card/sisitech_card.dart
+++ lib/sisitech_card/sisitech_card.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 
 class SisitechCard extends StatelessWidget {
@@ -10,61 +12,90 @@ class SisitechCard extends StatelessWidget {
   final double? imageScale; // Optional image scale
   final Color? iconColor; // Optional icon color
   final double? iconSize; // Optional icon size
+  final Color? titleColor;
+  final Color? descriptionColor;
 
-  const SisitechCard({
-    super.key,
-    this.assetImage, // Image path is optional
-    required this.description,
-    required this.title,
-    this.imageScale, // Image scale is optional
-    this.color = Colors.teal, // Default card color is teal
-    this.iconData, // Icon data is optional
-    this.iconColor, // Icon color is optional
-    this.iconSize, // Icon size is optional
-  });
+  const SisitechCard(
+      {super.key,
+      this.assetImage, // Image path is optional
+      required this.description,
+      required this.title,
+      this.imageScale, // Image scale is optional
+      this.color = Colors.teal, // Default card color is teal
+      this.iconData, // Icon data is optional
+      this.iconColor, // Icon color is optional
+      this.iconSize, // Icon size is optional
+      this.titleColor,
+      this.descriptionColor});
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: color,
-      child: Padding(
-        padding: const EdgeInsets.only(
-          top: 16.0,
-          bottom: 16.0,
-        ),
-        child: Column(
-          children: [
-            if (assetImage != null)
-              Image.asset(
-                assetImage!,
-                scale: imageScale ?? 1.0, // Use provided scale or default
-              )
-            else if (iconData != null)
-              Icon(
-                iconData,
-                size: iconSize ?? 24.0, // Use provided icon size or default
-                color: iconColor ??
-                    Colors.white, // Use provided icon color or default
+    return Column(
+      children: [
+        SizedBox(
+          width: Get.width,
+          child: Card(
+            color: color,
+            elevation: 2,
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Column(
+                children: [
+                  if (assetImage != null)
+                    Image.asset(
+                      assetImage!,
+                      scale: imageScale ?? 1.0,
+                    )
+                  else if (iconData != null)
+                    Icon(
+                      iconData,
+                      size: iconSize ?? 24.0,
+                      color: iconColor ?? Colors.white,
+                    ),
+                  SizedBox(
+                    height: Get.height * 0.006,
+                  ),
+                  Text(
+                    title,
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.titleSmall?.copyWith(
+                          color: titleColor,
+                        ),
+                  ),
+                  SizedBox(
+                    height: Get.height * 0.006,
+                  ),
+                  Text(
+                    description,
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                          color: descriptionColor,
+                        ),
+                  ),
+                ],
               ),
-            SizedBox(
-              height: Get.height * 0.006,
             ),
-            Text(
-              title,
-              textAlign: TextAlign.center,
-              style: Theme.of(context).textTheme.titleSmall,
-            ),
-            SizedBox(
-              height: Get.height * 0.006,
-            ),
-            Text(
-              description,
-              textAlign: TextAlign.center,
-              style: Theme.of(context).textTheme.titleLarge,
-            ),
-          ],
+          ),
         ),
-      ),
+        // Card(
+        //   color: Theme.of(context).primaryColor,
+        //   elevation: 2,
+        //   child: ListTile(
+        //     leading: Icon(Icons.label,
+        //         color: Theme.of(context).colorScheme.primaryContainer),
+        //     title: Text(
+        //       title,
+        //       style: TextStyle(
+        //           color: Theme.of(context).colorScheme.primaryContainer),
+        //     ),
+        //     subtitle: Text(
+        //       description,
+        //       style: TextStyle(
+        //           color: Theme.of(context).colorScheme.secondaryContainer),
+        //     ),
+        //   ),
+        // ),
+      ],
     );
   }
 }
