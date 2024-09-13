c67a6f8562a36ee36b95004b5e400405a1e2cadc
Ali Dennis <chivatsi@sisitech.com>
2024-09-13 14:53:49 +0300

    Fix anaonymous id

--- lib/mixpanel/mixpanel_controller.dart
+++ lib/mixpanel/mixpanel_controller.dart
@@ -73,7 +73,7 @@ class MixPanelController extends GetxController {
     }
 
     if (options.enableAnonymous) {
-      profile["username"] = generateMd5(anymousProfile["username"]!);
+      profile["username"] = generateMd5(profile["username"]!);
       dprint("Username: ${profile["username"]}");
     } else {
       dprint("Username: ${profile["username"]}");
